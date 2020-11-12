752a4a836d21923af890dca6c3e7559302cdb0d8
eugene <eugene.africansky@gmail.com>
2020-11-12 22:50:07 +0300

    add .gitignore file

---
+++ src/main/java/jdep/concurrency/Once.java
@@ -0,0 +1,40 @@
+package concurrency;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * Class allows to set a value once from different threads.
+ * Once value is set, any other set is ignored.
+ * @param <T>
+ *     Object that should be set once.
+ */
+public class Once<T> {
+
+    private final AtomicReference<Value<T>> value = new AtomicReference<>();
+
+    public boolean set(T value) {
+        return this.value.compareAndSet(null, new Value<>(value));
+    }
+
+    public T get() {
+        Value<T> v = value.get();
+        if (v == null) {
+            return null;
+        }
+        return v.get();
+    }
+
+    private class Value<T> {
+        private final T value;
+
+        public Value(T value) {
+            this.value = value;
+        }
+
+        public T get() {
+            return value;
+        }
+
+    }
+
+}
